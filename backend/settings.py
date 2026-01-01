02f999eae375614dccad0600285bab6840720939
KiahoreSK28 <kishoresenthamarai2004@gmail.com>
2026-01-01 23:10:04 +0530

    Finalize production settings and auto setup

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -164,3 +164,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+if os.getenv("AUTO_SETUP") == "True":
+    import subprocess
+    subprocess.run(["python", "manage.py", "migrate"])
+    subprocess.run(["python", "manage.py", "init_admin"])
+    subprocess.run(["python", "manage.py", "collectstatic", "--noinput"])
